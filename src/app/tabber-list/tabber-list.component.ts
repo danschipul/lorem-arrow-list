187499fd13362c4c22dc83294ff1bd24595bfc44
danschipul <danschipul@yahoo.com>
2018-06-06 23:32:27 -0400

    component cunstructor

--- src/app/tabber-list/tabber-list.component.ts
+++ src/app/tabber-list/tabber-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Tab } from '../tab';
+import { TabContentsService } from '../tab-contents.service';
+
 @Component({
   selector: 'app-tabber-list',
   templateUrl: './tabber-list.component.html',
   styleUrls: ['./tabber-list.component.css']
 })
+
 export class TabberListComponent implements OnInit {
 
-  constructor() { }
+	tabs: Tab[];
 
-  ngOnInit() {
-  }
+	tabsDOM: HTMLElement;
+
+	currentTab: number;
+
+	disabledTop: boolean;
+
+	disabledBottom: boolean;
+
+	//runs as soon as a class instance is on the page
+	constructor(private tabContentsService: TabContentsService) {
+		this.currentTab = -1;
+
+		//deals with bug for page refreshes when before something was tab focused
+		if ("activeElement" in document){
+			var ae:HTMLElement = <HTMLElement> document.activeElement;
+
+			ae.blur();
+		}
+	}
+
+	ngOnInit(): void {
+	}
 
 }
